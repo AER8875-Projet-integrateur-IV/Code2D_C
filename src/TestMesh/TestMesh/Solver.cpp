40c6a1f35d4d423635a308d34b7b93a7f347dcb6
charles D <charles19982009@hotmail.com>
2020-11-12 15:40:21 -0500

    structure du solver

---
+++ src/TestMesh/TestMesh/Solver.cpp
@@ -0,0 +1,32 @@
+//
+//  connectivity.cpp
+//  TestMesh
+//
+//  Created by Charles Dansereau on 2020-10-25.
+//
+
+#include "Solver_hpp"
+#include "MeshReader.cpp"
+#include "Metriques.cpp"
+
+using namespace std;
+
+double CalculateDeltat(double volume){
+  //calcul de delta t ici
+}
+
+double CalculateResidual(){
+  //calcul du r/sidu ici
+}
+
+vector<double> CalculateW(){
+  // Calcul de delta W
+}
+
+void UpdateW(){
+
+}
+
+void UpdateGhostsCells(){
+  
+}
---
+++ src/TestMesh/TestMesh/Solver.hpp
@@ -0,0 +1,20 @@
+//
+
+//
+//  Created by charles D. on 2020-10-25.
+//
+
+#ifndef Solver_hpp
+#define Solver_hpp
+
+#include <iostream>
+#include <vector>
+
+double CalculateDeltat(double volume);
+double CalculateResidual();
+vector<double> CalculateW();
+void UpdateW();
+void UpdateGhostsCells();
+
+
+#endif /* Solver_hpp */
